162ff2eb6fc738611c4b1afa72af057f827dbaff
Daniel Cruz <d.afc98@gmail.com>
2022-02-20 21:37:55 -0300

    avatar component

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Avatar } from "../avatar";
 import { styles } from "./styles";
 
 export function Profile() {
   return (
     <View style={styles.container}>
+
+      <Avatar urlImage="https://github.com/danielcruzdev.png"/>
+
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>

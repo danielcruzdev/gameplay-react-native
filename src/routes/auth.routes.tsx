ad168ab6b04e1dd8d66f2507f7eb8fd8f4c21b6d
Daniel Cruz <d.afc98@gmail.com>
2022-02-20 21:52:50 -0300

    navigation routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Home } from "../screens/home";
+import { SignIn } from "../screens/sign-in";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes(){
+    return (
+        <Navigator>
+            <Screen name="SignIn" component={SignIn}/>
+            <Screen name="Home" component={Home}/>
+        </Navigator>
+    )
+}

658c2d458631ed8e57dd05ee57df86714caeffa6
Daniel Cruz <d.afc98@gmail.com>
2022-02-28 19:58:34 -0300

    assets categories

--- src/components/category-select/index.tsx
+++ src/components/category-select/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { styles } from "./styles";
 
 export function CategorySelect() {
-  return (
-    <View style={styles.container}>
-    </View>
-  );
+    return (
+        <ScrollView 
+            horizontal 
+            showsHorizontalScrollIndicator={false} 
+            contentContainerStyle={{ paddingRight: 40 }} 
+            style={styles.container}>
+
+        </ScrollView>
+    );
 }

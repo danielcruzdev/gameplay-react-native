2a028382ef3abd5eadcca9033ae7cd846f112c8b
Daniel Cruz <d.afc98@gmail.com>
2022-02-28 20:52:50 -0300

    creating caregories list horizontal

--- src/components/category-select/index.tsx
+++ src/components/category-select/index.tsx
@@ -2,15 +2,32 @@ import React from "react";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { styles } from "./styles";
+import { categories } from "../../utils/categories";
+import { Category } from "../category";
 
-export function CategorySelect() {
+
+type Props = {
+    categorySelected: string
+}
+
+export function CategorySelect({ categorySelected }: Props) {
     return (
-        <ScrollView 
-            horizontal 
-            showsHorizontalScrollIndicator={false} 
-            contentContainerStyle={{ paddingRight: 40 }} 
-            style={styles.container}>
-
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingRight: 40 }}
+            style={styles.container}
+        >
+            {
+                categories.map(category => (
+                    <Category
+                        key={category.id}
+                        title={category.title}
+                        icon={category.icon}
+                        checked={category.id === categorySelected
+                        } />
+                ))
+            }
         </ScrollView>
     );
 }

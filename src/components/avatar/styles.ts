3ead1875af247d2e1b4d199703214513a53a3564
Daniel Cruz <d.afc98@gmail.com>
2022-03-01 15:12:07 -0300

    finalização do projeto NLW together app Gameplay

--- src/components/avatar/styles.ts
+++ src/components/avatar/styles.ts
@@ -1,14 +1,13 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../global/styles/theme";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
     width: 49,
     height: 49,
     borderRadius: 8,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 22,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 22
   },
   avatar: {
     width: 46,
--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,11 +1,11 @@
-import RankedSVG from "../assets/ranked.svg";
-import DuelSVG from "../assets/duel.svg";
-import FunSVG from "../assets/fun.svg";
-import TrainingSVG from "../assets/training.svg";
+import RankedSvg from '../assets/ranked.svg';
+import DuelSvg from '../assets/duel.svg';
+import FunSvg from '../assets/fun.svg';
+import TrainingSvg from '../assets/training.svg';
 
 export const categories = [
-  { id: "1", title: "Rankeada", icon: RankedSVG },
-  { id: "2", title: "Duelo 1x1", icon: DuelSVG },
-  { id: "3", title: "Diversão", icon: FunSVG },
-  { id: "4", title: "Treino", icon: TrainingSVG },
-];
+  { id: '1', title: 'Ranqueada', icon: RankedSvg },
+  { id: '2', title: 'Duelo 1x1', icon: DuelSvg },
+  { id: '3', title: 'Diversão', icon: FunSvg },
+  { id: '4', title: 'Treino', icon: TrainingSvg },
+]

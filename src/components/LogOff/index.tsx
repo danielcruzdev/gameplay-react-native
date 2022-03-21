4c80c1a74e8a4afbd3fc90bb2224b27cb3dbf8fa
Daniel Cruz <d.afc98@gmail.com>
2022-03-20 22:14:24 -0300

    create logoff button

---
+++ src/components/LogOff/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { styles } from './styles'
+
+export default class LogOff extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>LogOff</Text>
+      </View>
+    )
+  }
+}
